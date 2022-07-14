cd67ff8046e118a87dd66b92d4e62defb927a77a
Fernanda Fernandes do Nascimento <106761954+Fernandeezz@users.noreply.github.com>
2022-07-14 14:53:59 -0300

    Create Calculo Média Ponderada.py

---
+++ Calculo Média Ponderada.py
@@ -0,0 +1,5 @@
+n1 = float (input("Digite o primeiro número: "))
+n2= float (input("Digite o segundo número: "))
+n3= float (input("Digite o terceiro número: "))
+mA= (n1+n2+n3)/3 
+print ("A média aritmética é: %f"%mA)
