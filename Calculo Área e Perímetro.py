b59321536ed02cfa42111d5a79dfe7220cf48f2c
Fernanda Fernandes do Nascimento <106761954+Fernandeezz@users.noreply.github.com>
2022-07-14 14:50:50 -0300

    Create Calculo Área e Perímetro.py

---
+++ Calculo Área e Perímetro.py
@@ -0,0 +1,5 @@
+largura = float(input("digite a largura: "))
+altura = float (input("digite a altura: "))
+area = largura*altura
+per = 2*largura+2*altura
+print("Area: %f Perimetro: %f" %(area,per))
